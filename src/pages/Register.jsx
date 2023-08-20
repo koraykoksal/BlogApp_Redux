453e7c03e001460bf2b4e2bed312c8ddb9cc5936
koraykoksal <kksl.koray@gmail.com>
2023-08-20 14:32:51 +0300

    register page style

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,133 +1,87 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {FcGoogle} from 'react-icons/fc'
+import Container from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import Avatar from "@mui/material/Avatar"
+import LockIcon from "@mui/icons-material/Lock"
+import Grid from "@mui/material/Grid"
+import Box from "@mui/material/Box"
+import { Formik } from "formik"
+import RegisterForm, { registerSchema } from "../components/RegisterForm"
+import image from "../assets/result.svg"
+
 
 export const Register = () => {
   return (
     
-    <div className='flex flex-col items-center mb-20 mt-[100px]'>
-    <h5 className="flex items-center mb-6 text-2xl font-semibold text-red-700 dark:text-white">Register</h5>
 
+    <Container maxWidth="lg" sx={{mt:10}}>
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        rowSpacing={{ sm: 3 }}
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        {/* <Grid item xs={12}>
+          <Typography variant="h3" color="primary" align="center">
+            STOCK APP
+          </Typography>
+        </Grid> */}
 
-    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Create and account
-        </h1>
-        <form className="space-y-4 md:space-y-6" action="#">
-          <div>
-          <label
-              htmlFor="text"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Name"
-              required=""
-            />
-          </div>
-          <div>
-          <label
-              htmlFor="text"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Surname
-            </label>
-            <input
-              type="text"
-              name="surname"
-              id="surname"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Surname"
-              required=""
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Email"
-              required=""
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required=""
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="confirm-password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Confirm password
-            </label>
-            <input
-              type="confirm-password"
-              name="confirm-password"
-              id="confirm-password"
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required=""
-            />
-          </div>
-          <div className="flex items-start">
-            <div className="flex items-center h-5">
-              <input
-                id="terms"
-                aria-describedby="terms"
-                type="checkbox"
-                className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                required=""
-              />
-            </div>
-            <div className="ml-3 text-sm">
-              <label
-                htmlFor="terms"
-                className="font-light text-gray-500 dark:text-gray-300"
-              >
-                I accept the{" "}
-              </label>
-            </div>
-          </div>
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.light",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={2}
+            color="secondary.light"
+          >
+            Register
+          </Typography>
 
-          <button type='submit' className='w-full text-red-600 bg-slate-100 hover:bg-slate-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>Create an account</button>
+          <Formik
+            initialValues={{
+              username: "",
+              first_name: "",
+              last_name: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={registerSchema}
+            // onSubmit={(values, actions) => {
+            //   register({ ...values, password2: values.password })
+            //   actions.resetForm()
+            //   actions.setSubmitting(false)
+            // }}
+            component={(props) => <RegisterForm {...props} />}
+          ></Formik>
 
-          <button type='submit' className='w-full text-red-600 bg-slate-100 hover:bg-slate-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex items-center justify-center'>Continue With <FcGoogle size={'25px'}/> </button>
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/login">Do you have an account?</Link>
+          </Box>
+        </Grid>
 
-          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Already have an account?{" "}
-          
-            <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" to="/login">Login here</Link>
-          </p>
-        </form>
-      </div>
-    </div>
-  </div>
+        <Grid item xs={0} sm={7} md={6}>
+          <Container>
+            <img src={image} alt="" />
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
 
   )
 }
