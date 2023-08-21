411146817009435b67d70e892252b0aa07c4a138
koraykoksal <kksl.koray@gmail.com>
2023-08-21 09:03:48 +0300

    persistor

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,18 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux'
-import store from './app/store'
+import store, { persistor } from './app/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 function App() {
 
   return (
     <>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <AppRouter/>
+    </PersistGate>
     <ToastContainer/>
     </Provider>
 
