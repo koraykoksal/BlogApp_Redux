6611cef8c5902b24a11313c9ebf0cb9d78494cb0
koraykoksal <kksl.koray@gmail.com>
2023-08-22 13:51:37 +0300

    comment card style

--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -21,6 +21,7 @@ import { toastErrorNotify } from '../helper/ToastNotify';
 import noImage from '../assets/img/noImage.jpeg'
 import CommentIcon from '@mui/icons-material/Comment';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import { Box, Button, TextField } from '@mui/material';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -142,31 +143,39 @@ export const PostCards = ({item}) => {
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
+          <Typography paragraph variant='overline'>Comments</Typography>
+          
+          <Box sx={{display:'flex',flexDirection:'column',gap:2,mb:3}}>
+            <TextField 
+            required
+            placeholder='Comment..'
+            type='text'
+            name='comment'
+            id='comment'
+            />
+            <Button type='button' variant='contained'>Comment</Button>
+          </Box>
+
+          {
+            item.comments.map((com)=>(
+
+
+              <Card sx={{mb:1,boxShadow:3,border:'1px solid #A8DF8E'}}>
+                <CardContent sx={{display:'flex',flexDirection:'column',gap:1}}>
+                  <Typography sx={{fontSize:'14px'}} color={"text.secondary"}>
+                    {com.user}
+                  </Typography>
+                  <Typography sx={{fontSize:'14px'}}>
+                    {com.content}
+                  </Typography>
+                </CardContent>
+              </Card>
+
+            ))
+          }
+          
+          
+          
         </CardContent>
       </Collapse>
     </Card>
