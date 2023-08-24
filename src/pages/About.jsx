a76725a4998a35bde337734458d8a9b45d44046c
koraykoksal <kksl.koray@gmail.com>
2023-08-24 10:59:29 +0300

    desgined by text styling changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,13 +18,14 @@ export const About = () => {
       <Typography 
       width={250}
       sx={{
+        borderRadius:'0.3rem',
         color:'#ffffff',
         margin:'auto',
         mt:10,background: 'rgb(131,58,180)',
         background:'linear-gradient(45deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)'
         }} 
         variant='subtitle2'>
-          Designed By Koray Köksal.
+          ★ Designed By Koray Köksal ★
       </Typography>
 
       </Grid>
